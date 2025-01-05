55490e207ac643f7d617877e8d1cbcab2906286a
shuritk1712 <shrutikdawane@gmail.com>
2025-01-05 16:43:04 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
 import BannerSlider from './components/BannerSlider';
 import Vendors from './components/Vendors';
 import Awards from './components/Awards';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
-import Footer from './Footer';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-50">
-      
-      <main>
       <Navbar />
+      <main>
         <BannerSlider />
         <Vendors />
         <Awards />
         <Contact />
-        <Footer/>
       </main>
+      <Footer />
     </div>
   );
 };
